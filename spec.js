c85c2ab9b992e5bca5a1c278abd862ecbf77fffb
JakobKallin <jakobkallin>
2012-07-22 01:13:55 +0200

    Failing test showing that Knockout requires nodes to be added to document

--- spec.js
+++ spec.js
@@ -8,4 +8,18 @@ describe('Knockout', function() {
 		ko.applyBindings(viewModel, node);
 		expect(node.textContent).toBe('John');
 	});
+	
+	it('supports change notification for nodes not added to document', function() {
+		var viewModel = {
+			name: ko.observable('John')
+		};
+		
+		var node = document.createElement('div');
+		node.dataset.bind = 'text: name';
+		ko.applyBindings(viewModel, node);
+		
+		expect(node.textContent).toBe('John');
+		viewModel.name('James');
+		expect(node.textContent).toBe('James');
+	});
 });
