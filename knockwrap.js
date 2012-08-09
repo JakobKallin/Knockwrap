565d56713fff29653710ee88b8a5cba00b0503be
JakobKallin <jakobkallin>
2012-08-09 02:24:29 +0200

    Mutates computed properties

--- knockwrap.js
+++ knockwrap.js
@@ -1,5 +1,27 @@
+Object.getPropertyDescriptor = function(target, property) {
+	var descriptor = Object.getOwnPropertyDescriptor(target, property);
+	var proto = Object.getPrototypeOf(target);
+	
+	if ( descriptor ) {
+		return descriptor;
+	} else if ( proto ) {
+		return Object.getPropertyDescriptor(proto, property);
+	} else {
+		return undefined;
+	}
+};
+
 knockwrap = function() {
 	function wrapProperty(target, property) {
+		var descriptor = Object.getPropertyDescriptor(target, property);
+		if ( descriptor.get ) {
+			wrapGetter(target, property);
+		} else {
+			wrapSimpleProperty(target, property);
+		}
+	}
+	
+	function wrapSimpleProperty(target, property) {
 		var observable = ko.observable(target[property]);
 		var getter = function() {
 			return observable();
@@ -10,6 +32,19 @@ knockwrap = function() {
 		Object.defineProperty(target, property, {
 			get: getter,
 			set: setter
+		});
+	}
+	
+	function wrapGetter(target, property) {
+		var descriptor = Object.getPropertyDescriptor(target, property);
+		var originalGetter = descriptor.get;
+		var computed = ko.computed(originalGetter, target);
+		var wrappedGetter = function() {
+			return computed();
+		};
+		
+		Object.defineProperty(target, property, {
+			get: wrappedGetter
 		});
 	}
 	
--- spec.js
+++ spec.js
@@ -7,4 +7,20 @@ describe('Knockwrap', function() {
 		target.name = 'John';
 		expect(target.name).toBe('John');
 	});
+	
+	it('mutates computed properties', function() {
+		var target = {
+			first: 'James',
+			last: 'Smith',
+			get full() {
+				return this.first + ' ' + this.last;
+			}
+		};
+		knockwrap.wrapProperty(target, 'first');
+		knockwrap.wrapProperty(target, 'last');
+		knockwrap.wrapProperty(target, 'full');
+		
+		target.first = 'Robert';
+		expect(target.full).toBe('Robert Smith');
+	});
 });
